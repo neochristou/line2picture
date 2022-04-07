d7b32d638b8391ac931651eadc9fa2ae7b420c87
Neophytos Christou <neophytos.christou19@gmail.com>
2022-04-07 12:24:36 -0400

    Script for creating outlines for input images

--- src/create_outlines.py
+++ src/create_outlines.py
@@ -1,13 +1,16 @@
-import sys
+import glob
 
 import cv2
 import numpy as np
 
-original = cv2.imread(sys.argv[1])
+for filename in glob.glob("../data/*jpg"):
 
-gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-gray_smooth = cv2.bilateralFilter(gray, 7, 60, 60)
-outline = cv2.Canny(gray_smooth, 60, 120)
-gray_outline = cv2.add(gray, outline)
-outline_3_channel = np.stack((outline,) * 3, axis=-1)
-cv2.imwrite("output.png", outline_3_channel)
+    print(filename)
+    original = cv2.imread(filename)
+    gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    gray_smooth = cv2.bilateralFilter(gray, 7, 60, 60)
+    outline = cv2.Canny(gray_smooth, 60, 120)
+    gray_outline = cv2.add(gray, outline)
+    outline_3_channel = np.stack((outline,) * 3, axis=-1)
+    out_filename = filename.replace(".jpg", "_outline.jpg")
+    cv2.imwrite(out_filename, outline_3_channel)
